088953b8fdd7209044e070eb3f5d2494805bd12b
Leif Niemczik <leif.niem@gmail.com>
2022-11-18 16:09:51 +0100

    visual gen with overflow bugs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+import compress from "astro-compress";
+
+export default defineConfig({
+  integrations: [compress()]
+});
